f2d09784bc6c0c7e1abde2d9a9147b75bf319565
anates <rolandar@stud.ntnu.no>
2022-12-28 13:19:26 +0100

    Linting fixes, added header for copyright

--- filter_mirrors.py
+++ filter_mirrors.py
@@ -1,61 +1,62 @@
 #!/usr/bin/env python3
-
-import h5py
+#
+# Created on May 25 2022
+#
+# Created by Roland Axel Richter -- roland.richter@empa.ch
+#
+# Copyright (c) 2022
+from typing import Union
 import numpy as np
-import scipy.constants as scco
-from scipy.signal import butter, lfilter, freqz
-from scipy.integrate import solve_ivp
 from scipy.interpolate import interp1d
 import matplotlib
-matplotlib.use('Qt5Cairo')
-from matplotlib import pyplot as plt
-from scipy import interpolate
 
-import math
-from string import Template
-import subprocess
+matplotlib.use("Qt5Cairo")
 
-import types
-import functools
 
-from enum import Enum
+class MirrorFilterILMA:
+    """_summary_"""
 
-#mirror_spectrum_file = "/home/roland/Dokumente/Uni/Verstärker-Tests/109612 ASCII.csv"
-#mirror_II_spectrum_file = "/home/roland/Dokumente/Uni/Verstärker-Tests/1800 mirror.csv"
-#import mirror data
-'''
-mirror_wl_data = []
-mirror_ref_data = []
-mirror_selection = 0
-with open(mirror_spectrum_file, 'r') as f:
-    data_lines = f.readlines()
-    for line_number, line in enumerate(data_lines):
-        data = line.split(',')
-        if len(data) == 5 and line_number > 1:
-            mirror_wl_data.append(float(data[0]))
-            mirror_ref_data.append(float(data[mirror_selection + 1]))
-mirror_wl_data = np.flip(np.asarray(mirror_wl_data))
-mirror_ref_data = np.flip(np.asarray(mirror_ref_data)) / 100
-'''
-
-class mirror_filter_I_LMA:
-    def __init__(self):
-        self.mirror_data_file = "/home/roland/Dokumente/Uni/Verstärker-Tests/109612 ASCII.csv"
+    def __init__(self) -> None:
+        """__init__ _summary_"""
+        self.mirror_data_file = "109612 ASCII.csv"
         self.scaling_factor = 1
         self.wl_shift = 7
         self.name = "First mirror"
         self.line_parameters = [5, 1]
 
-class mirror_filter_II_LMA:
-    def __init__(self):
-        self.mirror_data_file = "/home/roland/Dokumente/Uni/Verstärker-Tests/1800 mirror.csv"
+
+class MirrorFilterIILMA:
+    """_summary_"""
+
+    def __init__(self) -> None:
+        """__init__ _summary_"""
+        self.mirror_data_file = "1800 mirror.csv"
         self.scaling_factor = 95 / 58
         self.wl_shift = -6
         self.name = "Second mirror"
         self.line_parameters = [3, 2, 2700]
 
-class filter_mirror:
-    def __init__(self, mirror_data, scaling_factor, wl_shift, name, line_parameters = []):
+
+class FilterMirror:
+    """_summary_"""
+
+    def __init__(
+        self,
+        mirror_data: str,
+        scaling_factor: float,
+        wl_shift: float,
+        name: str,
+        line_parameters: Union[tuple[float, float], tuple[float, float, float]],
+    ) -> None:
+        """__init__ _summary_
+
+        Args:
+            mirror_data (_type_): _description_
+            scaling_factor (_type_): _description_
+            wl_shift (_type_): _description_
+            name (_type_): _description_
+            line_parameters (list, optional): _description_. Defaults to [].
+        """
         self.mirror_data_file = mirror_data
         self.scaling_factor = scaling_factor
         self.wl_shift = wl_shift
@@ -64,31 +65,65 @@ class filter_mirror:
         self.create_mirror()
 
     @classmethod
-    def fromMirrorClass(filter_mirror, mirror_template):
-        return filter_mirror(mirror_template.mirror_data_file, mirror_template.scaling_factor, mirror_template.wl_shift, mirror_template.name, mirror_template.line_parameters)
+    def from_mirror_class(cls, mirror_template) -> FilterMirror:
+        """fromMirrorClass _summary_
 
-    def apply_mirror(self, wl_data, meas_data):
+        Args:
+            filter_mirror (_type_): _description_
+            mirror_template (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        return FilterMirror(
+            mirror_template.mirror_data_file,
+            mirror_template.scaling_factor,
+            mirror_template.wl_shift,
+            mirror_template.name,
+            mirror_template.line_parameters,
+        )
+
+    def apply_mirror(self, wl_data, meas_data) -> tuple[np.ndarray, np.ndarray]:
+        """apply_mirror _summary_
+
+        Args:
+            wl_data (_type_): _description_
+            meas_data (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
         ret_meas_data = []
         for i, elem in enumerate(wl_data):
             ret_meas_data.append(self.mirror_interpolator(elem) * meas_data[i])
         return (np.asarray(wl_data), np.asarray(ret_meas_data))
 
-    def create_mirror(self):
+    def create_mirror(self) -> None:
+        """create_mirror _summary_"""
         mirror_wl_data = []
         mirror_ref_data = []
-        with open(self.mirror_data_file, 'r') as f:
-            data_lines = f.readlines()
+        with open(self.mirror_data_file, "r", encoding="utf-8") as mirror_file_reader:
+            data_lines = mirror_file_reader.readlines()
             for line_number, line in enumerate(data_lines):
-                data = line.split(',')
+                data = line.split(",")
                 if len(self.line_parameters) == 2:
-                    if len(data) == self.line_parameters[0] and line_number > self.line_parameters[1]:
+                    if (
+                        len(data) == self.line_parameters[0]
+                        and line_number > self.line_parameters[1]
+                    ):
                         mirror_wl_data.append(float(data[0]) + self.wl_shift)
                         mirror_ref_data.append(float(data[1]))
                 else:
                     if len(self.line_parameters) == 3:
-                        if len(data) == self.line_parameters[0] and line_number > self.line_parameters[1] and line_number < self.line_parameters[2]:
+                        if (
+                            len(data) == self.line_parameters[0]
+                            and line_number > self.line_parameters[1]
+                            and line_number < self.line_parameters[2]
+                        ):
                             mirror_wl_data.append(float(data[0]) + self.wl_shift)
                             mirror_ref_data.append(float(data[1]))
         self.mirror_wl_data = np.flip(np.asarray(mirror_wl_data))
-        self.mirror_ref_data = 1 / (1 - self.scaling_factor * np.flip(np.asarray(mirror_ref_data)) / 100)
+        self.mirror_ref_data = 1 / (
+            1 - self.scaling_factor * np.flip(np.asarray(mirror_ref_data)) / 100
+        )
         self.mirror_interpolator = interp1d(self.mirror_wl_data, self.mirror_ref_data)
--- laser_diode.py
+++ laser_diode.py
@@ -1,82 +1,151 @@
 #!/usr/bin/env python3
+#
+# Created on May 25 2022
+#
+# Created by Roland Axel Richter -- roland.richter@empa.ch
+#
+# Copyright (c) 2022
+""" _summary_
 
-import h5py
-import numpy as np
-import scipy.constants as scco
-from scipy.signal import butter, lfilter, freqz, filtfilt
-from scipy.integrate import solve_ivp
-from matplotlib import pyplot as plt
-from scipy.optimize import curve_fit
+Returns:
+    _type_: _description_
+"""
 from scipy import interpolate
-import tikzplotlib
+import numpy as np
 
-import math
-from string import Template
-import subprocess
-
-import types
-import functools
-
-from enum import Enum
 
 class LaserDiode:
-    def __init__(self, current_points, power_points):
+    """_summary_"""
+
+    def __init__(self, current_points: np.ndarray, power_points: np.ndarray) -> None:
+        """__init__ _summary_
+
+        Args:
+            current_points (np.ndarray): _description_
+            power_points (np.ndarray): _description_
+        """
         self.current_points = current_points
         self.power_points = power_points
-        self.power_function = interpolate.interp1d(self.current_points, self.power_points, kind='linear', fill_value="extrapolate")
+        self.power_function = interpolate.interp1d(
+            self.current_points,
+            self.power_points,
+            kind="linear",
+            fill_value="extrapolate",
+        )
 
-    def get_power(self, input_current):
+    def get_power(self, input_current: float) -> float:
+        """get_power _summary_
+
+        Args:
+            input_current (_type_): _description_
+
+        Returns:
+            float: _description_
+        """
         power = self.power_function(input_current)
         if power < 0:
-            return 0
+            return 0.0
         else:
             return self.power_function(input_current)
 
-    def get_power_from_voltage(self, input_voltage): # if LD is based on voltage
+    def get_power_from_voltage(
+        self, input_voltage: float
+    ) -> float:  # if LD is based on voltage
+        """get_power_from_voltage _summary_
+
+        Args:
+            input_voltage (_type_): _description_
+
+        Returns:
+            float: _description_
+        """
         return self.get_power(input_voltage / 0.8)
 
+
 class DILAS_I(LaserDiode):
-    def __init__(self):
-        measured_current_points = [10, 12.5, 15, 17.5, 20]
-        measured_power_points = [1630, 3220, 4770, 6380, 7930]
+    """DILAS_I _summary_
+
+    Args:
+        LaserDiode (_type_): _description_
+    """
+
+    def __init__(self) -> None:
+        """__init__ _summary_"""
+        measured_current_points = np.array([10, 12.5, 15, 17.5, 20], dtype=np.float64)
+        measured_power_points = np.array(
+            [1630, 3220, 4770, 6380, 7930], dtype=np.float64
+        )
         super().__init__(measured_current_points, measured_power_points)
         self.name = "Dilas Diode I"
 
-    def get_power(self, input_current):
-        return super().get_power(input_current)
 
 class DILAS_II(LaserDiode):
-    def __init__(self):
-        #measured_current_points = [10, 12.5, 15, 17.5, 20]
-        #measured_power_points = [1830, 3400, 4940, 6510, 8040]
-        measured_current_points = [8, 10, 12, 14, 16, 18, 20]
-        measured_power_points = [258, 1978, 3698, 5418, 7138, 8858, 10578]
+    """DILAS_II _summary_
+
+    Args:
+        LaserDiode (_type_): _description_
+    """
+
+    def __init__(self) -> None:
+        """__init__ _summary_"""
+        measured_current_points = np.array(
+            [8, 10, 12, 14, 16, 18, 20], dtype=np.float64
+        )
+        measured_power_points = np.array(
+            [258, 1978, 3698, 5418, 7138, 8858, 10578], dtype=np.float64
+        )
         super().__init__(measured_current_points, measured_power_points)
         self.name = "Dilas Diode II"
 
-    def get_power(self, input_current):
-        return super().get_power(input_current)
 
 class DILAS_III(LaserDiode):
-    def __init__(self):
-        #measured_current_points = [10, 12.5, 15, 17.5, 20]
-        #measured_power_points = [2170, 3970, 5800, 7510, 9300]
-        measured_current_points = [8, 10, 12, 14, 16, 18, 20]
-        measured_power_points = [570, 2470, 4370, 6270, 8170, 10070, 11970]
+    """DILAS_III _summary_
+
+    Args:
+        LaserDiode (_type_): _description_
+    """
+
+    def __init__(self) -> None:
+        """__init__ _summary_"""
+        measured_current_points = np.array(
+            [8, 10, 12, 14, 16, 18, 20], dtype=np.float64
+        )
+        measured_power_points = np.array(
+            [570, 2470, 4370, 6270, 8170, 10070, 11970], dtype=np.float64
+        )
         super().__init__(measured_current_points, measured_power_points)
         self.name = "Dilas Diode III"
 
-    def get_power(self, input_current):
-        return super().get_power(input_current)
-        
+
 class nLight(LaserDiode):
-    def __init__(self):
-        measured_current_points = [0.720, 0.785, 0.845, 0.910]#, 1.13, 1.41, 1.69, 1.96, 2.24]
-        measured_power_points = [20, 252, 615, 990]#, 1860, 3120, 4390, 5610, 6820]
+    """nLight _summary_
+
+    Args:
+        LaserDiode (_type_): _description_
+    """
+
+    def __init__(self) -> None:
+        """__init__ _summary_"""
+        measured_current_points = np.array(
+            [
+                0.720,
+                0.785,
+                0.845,
+                0.910,
+            ],
+            dtype=np.float64,
+        )
+        measured_power_points = np.array([20, 252, 615, 990], dtype=np.float64)
         super().__init__(measured_current_points, measured_power_points)
         self.name = "nLight-Diode"
 
-    def get_power(self, input_voltage):
+    def get_power(self, input_voltage: float) -> float:
+        """get_power _summary_
+
+        Args:
+            input_voltage (_type_): _description_
+
+        Returns:
+            float: _description_
+        """
         return super().get_power(input_voltage / 0.8)
-
-
